eef5cc211370caf920d3d469c88fded6bd2149d1
KoungQ <guu119@naver.com>
2025-09-08 20:43:54 +0900

    init

---
+++ src/main/java/com/project/sleep/SleepServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.sleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/sleep/SleepServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sleep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
