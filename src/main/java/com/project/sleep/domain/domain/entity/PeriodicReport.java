8c668a600a3aa5c38d7ba93129dfb09e3a11e2b6
KoungQ <guu119@naver.com>
2025-09-22 15:30:43 +0900

    chore: 타입 변경

--- src/main/java/com/project/sleep/domain/domain/entity/PeriodicReport.java
+++ src/main/java/com/project/sleep/domain/domain/entity/PeriodicReport.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -31,7 +32,7 @@ public class PeriodicReport {
     private int totalSleepTime;
 
     @Field("bed_time")
-    private String bedTime;
+    private LocalDateTime bedTime;
 
     @Field("deep_sleep_ratio")
     private double deepSleepRatio;
