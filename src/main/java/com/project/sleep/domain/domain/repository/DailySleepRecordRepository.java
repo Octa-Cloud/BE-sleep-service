bba69d6c026fa974ccb17a8938ae9976f8e61774
yukyoungs <syk9848@gmail.com>
2025-10-09 20:07:13 +0900

    limit 8

--- src/main/java/com/project/sleep/domain/domain/repository/DailySleepRecordRepository.java
+++ src/main/java/com/project/sleep/domain/domain/repository/DailySleepRecordRepository.java
@@ -15,14 +15,8 @@ public interface DailySleepRecordRepository extends MongoRepository<DailySleepRe
     Optional<DailySleepRecord> findByUserNoAndSleepDate(Long userNo, LocalDate sleepDate);
 
     // 특정 사용자의 최근 8개 수면 기록 조회 (프로젝션 적용)
-    @Query(value = "{ 'userNo' : ?0 }", sort = "{ 'sleepDate' : -1 }", fields = "{ 'sleepDate' : 1, 'score' : 1, 'totalSleepTime' : 1, 'bedTime' : 1, 'wakeTime' : 1 }")
+    // 쿼리 메서드 규칙과 @Query 어노테이션 충돌 이슈(limit(8)를 무시함)로 제거
+    //@Query(value = "{ 'userNo' : ?0 }", sort = "{ 'sleepDate' : -1 }", fields = "{ 'sleepDate' : 1, 'score' : 1, 'totalSleepTime' : 1, 'bedTime' : 1, 'wakeTime' : 1 }")
     List<DailySleepRecord> findTop8ByUserNoOrderBySleepDateDesc(Long userNo);
 
-//    @Query("{ 'user_no': ?0, 'sleep_date': { $gte: ?1, $lte: ?2 } }")
-//    List<DailySleepRecord> findByUserNoAndSleepDateBetween(Long userNo, LocalDate startDate, LocalDate endDate);
-
-    // 기존 메서드 (사용하지 않음, 필요에 따라 삭제 가능)
-    List<DailySleepRecord> findTop8BySleepDateLessThanEqualOrderBySleepDateDesc(LocalDate date);
-    DailySleepRecord findBySleepDate(LocalDate date);
-    // 안쓰게되면 삭제해주세요.
 }
